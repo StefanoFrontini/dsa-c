f3930602254a86b9454103768eb6fb2216ae7356
Stefano Frontini <stefanofrontini75@gmail.com>
2026-01-16 00:10:49 +0100

    bst

--- antirez/bst2.c
+++ antirez/bst2.c
@@ -24,18 +24,6 @@ void print_node(struct node *n) {
   print_node(n->right);
 }
 
-void add_node(struct node **root, int val) {
-  if ((*root) == NULL) {
-    struct node *new = create_node(val);
-    *root = new;
-    return;
-  }
-  if (val < (*root)->val) {
-    add_node(&(*root)->left, val);
-  } else {
-    add_node(&(*root)->right, val);
-  }
-}
 void free_tree(struct node *root) {
   if (root == NULL)
     return;
@@ -43,13 +31,28 @@ void free_tree(struct node *root) {
   free_tree(root->right);
   free(root);
 }
+struct node *add_node(struct node *root, int val) {
+  if (root == NULL) {
+    struct node *new = create_node(val);
+    return new;
+  }
+
+  if (val < root->val) {
+    root->left = add_node(root->left, val);
+  } else if (val > root->val) {
+    root->right = add_node(root->right, val);
+  }
+  return root;
+}
 
 int main(void) {
   struct node *root = NULL;
-  add_node(&root, 10);
-  add_node(&root, 5);
-  add_node(&root, 12);
-
+  root = add_node(root, 10);
+  printf("%p\n", root);
+  root = add_node(root, 5);
+  printf("%p\n", root);
+  root = add_node(root, 12);
+  printf("%p\n", root);
   print_node(root);
   free_tree(root);
   return 0;
