e48b671606fafaff5c9ea2b5b6dc537670e2f19f
Stefano Frontini <stefanofrontini75@gmail.com>
2026-01-26 00:07:16 +0100

    snake v2

--- antirez/snake.c
+++ antirez/snake.c
@@ -1,24 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 // #include <unistd.h>
 
 // typedef struct {
 //   uint8_t row;
 //   uint8_t column;
 // } grid;
-#define GRID_ROWS 25
-#define GRID_COLS 25
+#define GRID_ROWS 15
+#define GRID_COLS 30
 #define GRID_CELLS (GRID_ROWS * GRID_COLS)
-#define GROUND "."
-#define BODY "*"
-
-typedef struct {
-  char x;
-  char y;
-} coord;
+#define GROUND '.'
+#define BODY '*'
 
 typedef struct node {
-  coord value;
+  char x;
+  char y;
   struct node *next;
   struct node *prev;
 
@@ -29,6 +26,16 @@ typedef struct {
   node *tail;
 
 } snake;
+
+void setCell(char *grid, char x, char y, char state) {
+  int index = x + GRID_COLS * y;
+  grid[index] = state;
+}
+
+char getCell(char *grid, char x, char y) {
+  int index = x + GRID_COLS * y;
+  return grid[index];
+}
 
 // void setGrid(int x, int y, char state) { int index = y * GRID_COLS + x; }
 
@@ -42,8 +49,8 @@ typedef struct {
 node *createNode(char x, char y) {
   node *new = malloc(sizeof(node));
 
-  new->value.x = x;
-  new->value.y = y;
+  new->x = x;
+  new->y = y;
   new->next = NULL;
   new->prev = NULL;
 
@@ -51,27 +58,30 @@ node *createNode(char x, char y) {
 }
 // void freeNode(node *n) { free(n); }
 
-snake *createSnake(char x, char y) {
+snake *createSnake(char x, char y, char *grid) {
   node *newNode = malloc(sizeof(node));
-  newNode->value.x = x;
-  newNode->value.y = y;
+  newNode->x = x;
+  newNode->y = y;
   newNode->next = NULL;
   newNode->prev = NULL;
   snake *s = malloc(sizeof(snake));
   s->head = newNode;
   s->tail = newNode;
+  setCell(grid, x, y, BODY);
   return s;
 }
-snake *prepend(snake *s, char x, char y) {
+snake *prepend(snake *s, char x, char y, char *grid) {
   node *newNode = createNode(x, y);
   s->head->prev = newNode;
   newNode->next = s->head;
   s->head = newNode;
+  setCell(grid, x, y, BODY);
   return s;
 }
-snake *deleteTail(snake *s) {
+snake *deleteTail(snake *s, char *grid) {
   node *newTail = s->tail->prev;
   newTail->next = NULL;
+  setCell(grid, s->tail->x, s->tail->y, GROUND);
   free(s->tail);
   s->tail = newTail;
   return s;
@@ -81,9 +91,17 @@ void printNode(node *head) {
   if (head == NULL)
     return;
   while (head != NULL) {
-    printf("(%d, %d) (head: %p) (next: %p, prev: %p)\n", head->value.x,
-           head->value.y, head, head->next, head->prev);
+    printf("(%d, %d) (head: %p) (next: %p, prev: %p)\n", head->x, head->y, head,
+           head->next, head->prev);
     head = head->next;
+  }
+}
+void printGrid(char *grid) {
+  for (int y = 0; y < GRID_ROWS; y++) {
+    for (int x = 0; x < GRID_COLS; x++) {
+      printf("%c", getCell(grid, x, y));
+    }
+    printf("\n");
   }
 }
 
@@ -92,14 +110,19 @@ int main(void) {
   //   grid snakeGrid;
   //   snakeGrid.row = 25;
   //   snakeGrid.column = 25;
-  //   char grid[GRID_CELLS];
-  snake *snake = createSnake(12, 12);
-  snake = prepend(snake, 15, 16);
-  snake = prepend(snake, 18, 19);
+  char grid[GRID_CELLS];
+  memset(grid, GROUND, GRID_CELLS);
+  snake *snake = createSnake(0, 0, grid);
+  snake = prepend(snake, 1, 0, grid);
+  snake = prepend(snake, 2, 0, grid);
   printNode(snake->head);
-  snake = deleteTail(snake);
+  //   snake = deleteTail(snake, grid);
   printf("-------------------------\n");
   printNode(snake->head);
+  printf("-------------------------\n");
+  //   setCell(grid, 24, 24, BODY);
+  //   setCell(grid, 0, 0, BODY);
+  printGrid(grid);
   //   initSnake();
   //   initGrid(grid, GROUND);
 
