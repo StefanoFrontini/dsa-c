a9440c79d402dac238306932a038a78f879f0f9f
Stefano Frontini <stefanofrontini75@gmail.com>
2026-03-27 13:14:41 +0100

    sy4

--- antirez/math-shunting-yard/math-shunting-yard.c
+++ antirez/math-shunting-yard/math-shunting-yard.c
@@ -7,6 +7,9 @@
 #include <unistd.h>
 
 #define LIST_CAPACITY 1
+#define SY_OK 0
+#define SY_ERR 1
+
 
 /* ------------------------ Allocation wrappers ----------------------------*/
 
@@ -28,7 +31,6 @@ void *xrealloc(void *old_ptr, size_t size) {
   return ptr;
 }
 
-
 /* ------------------    Data structure Shunting-yard object ---------------- */
 
 typedef enum { NUMBER = 0, SYMBOL, LIST } SyObjType;
@@ -40,13 +42,16 @@ typedef struct SyObj {
     char symbol;
     int num;
     struct {
-      SyObj **ele;
+      struct SyObj **ele;
       size_t len;
       size_t capacity;
     } list;
   };
 } SyObj;
 
+/* -------------  Functions prototypes ------------------------- */
+void retain(SyObj *o);
+void release(SyObj *o);
 
 /*---------------     Object related functions ----------------  */
 
@@ -80,44 +85,71 @@ SyObj *createListObject(void) {
   return o;
 }
 
+/* Free an object and all the other nested objects. */
+void freeObject(SyObj *o) {
+  switch (o->type) {
+    case LIST:
+      for (size_t i = 0; i < o->list.len; i++) {
+        SyObj *el = o->list.ele[i];
+        release(el);
+      }
+      free(o->list.ele);
+      break;
+    default:
+      break;
+  }
+  free(o);
+}
+
+void retain(SyObj *o) {
+  o->refcount++;
+}
+
+void release(SyObj *o) {
+  assert(o->refcount > 0);
+  o->refcount--;
+  if (o->refcount == 0) freeObject(o);
+}
+
+
 void printSyObj(SyObj *o) {
   switch (o->type) {
     case NUMBER:
-      printf("%d ",o->num);
+      printf("%d ", o->num);
       break;
     case SYMBOL:
-      printf("%c ",o->symbol);
+      printf("%c ", o->symbol);
       break;
     case LIST:
-      for (size_t i = 0; i < o->list.len; i++){
+      for (size_t i = 0; i < o->list.len; i++) {
         printSyObj(o->list.ele[i]);
       }
       break;
 
     default:
+      printf("?\n");
       break;
-
-
   }
 }
 
-void listPush(SyObj *l, SyObj *o){
-    if(l->list.len >= l->list.capacity - 1){
-        l->list.ele = xrealloc(l->list.ele, l->list.capacity * 2);
-        l->list.capacity = l->list.capacity * 2;
-    }
-    l->list.ele[l->list.len] = o;
-    l->list.len++;
-
+void listPush(SyObj *l, SyObj *o) {
+  if (l->list.len >= l->list.capacity) {
+    size_t newCap = l->list.capacity * 2;
+    l->list.ele = xrealloc(l->list.ele, newCap * sizeof(SyObj *));
+    l->list.capacity = newCap;
+  }
+  l->list.ele[l->list.len] = o;
+  l->list.len++;
 }
 
-SyObj *listPop(SyObj *l){
-    if(l->list.len > 0){
-        SyObj *o = l->list.ele[l->list.len - 1];
-        l->list.len--;
-        return o;
-    }
-
+SyObj *listPop(SyObj *l) {
+  if (l->list.len > 0) {
+    SyObj *o = l->list.ele[l->list.len - 1];
+    l->list.len--;
+    return o;
+  } else {
+    return NULL;
+  }
 }
 
 typedef struct SyParser {
@@ -131,37 +163,102 @@ typedef struct SyCtx {
 
 } SyCtx;
 
-/* -------------------------  Main ----------------------------- */
+/* ----------- Turn program into shunting yard list ---------- */
 
-int main(int argc, char **argv){
-    if(argc !=2){
-        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
-        return 1;
+SyObj *parseNumber(SyParser *parser) {
+  char buf[128];
+  char *start = parser->p;
+  char *end;
+  if (*parser->p == '-') parser->p++;
+
+  while (*parser->p && isdigit(*parser->p))
+    parser->p++;
+
+  end = parser->p;
+  size_t len = end - start;
+
+if (len >= sizeof(buf)) {
+    fprintf(stderr, "Number literal too long\n");
+    return NULL;
+  }
+  memcpy(buf, start, len);
+  buf[len] = 0;
+  return createNumberObject(atoi(buf));
+}
+/* Return true if the character 'c' is one of the characters acceptable for our
+ * symbols. */
+int isSymbolChar(int c) {
+  char symchars[] = "+-*/%";
+  return isalpha(c) || strchr(symchars, c) != NULL;
+}
+
+SyObj *parseSymbol(SyParser *parser) {
+  char s = *parser->p;
+  parser->p++;
+  return createSymbolObject(s);
+}
+
+void parseSpaces(SyParser *parser) {
+  while (isspace(*parser->p)) {
+    parser->p++;
+  }
+}
+
+SyObj *parse(char *prg) {
+  SyParser parser;
+  parser.prg = prg;
+  parser.p = prg;
+  SyObj *parsed = createListObject();
+  SyObj *o;
+  while (*parser.p) {
+    parseSpaces(&parser);
+    if (*parser.p == 0) break;
+    if (isdigit(*parser.p) || (*parser.p == '-' && isdigit(parser.p[1]))) {
+      o = parseNumber(&parser);
+    } else if (isSymbolChar(*parser.p)) {
+      o = parseSymbol(&parser);
+    } else {
+      o = NULL;
     }
 
-    FILE *fp = fopen(argv[1], "r");
+    // Check if the current token produced a parsing error.
+    if (o == NULL) {
+      release(parsed);
+      fprintf(stderr, "Error parsing\n");
+      return NULL;
+    } else {
+      listPush(parsed, o);
+    }
+  }
+  return parsed;
+}
+
+/* -------------------------  Main ----------------------------- */
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+    return 1;
+  }
+
+  FILE *fp = fopen(argv[1], "r");
 
   if (fp == NULL) {
     fprintf(stderr, "Unable to open the file\n");
     return 1;
   }
 
-    fseek(fp, 0, SEEK_END);
-    long file_size = ftell(fp);
-    fseek(fp, 0, SEEK_SET);
-    char *buf = xmalloc(file_size + 1);
-    fread(buf, file_size, 1, fp);
-    buf[file_size] = 0;
-    printf("text is: %s\n", buf);
-    fclose(fp);
-    SyParser p;
-    p.prg = buf;
-    p.p = buf;
-    // readEndOfFile(&l);
-    // advanceLexer(&l);
-    // CsObj *parsed = parseSong(&l);
-    // printCsObj(parsed);
-    // releaseCsObj(parsed);
-    free(buf);
-    return 0;
+  fseek(fp, 0, SEEK_END);
+  long file_size = ftell(fp);
+  fseek(fp, 0, SEEK_SET);
+  char *buf = xmalloc(file_size + 1);
+  fread(buf, file_size, 1, fp);
+  buf[file_size] = 0;
+  printf("text is: %s\n", buf);
+  fclose(fp);
+  SyObj *parsed = parse(buf);
+  printSyObj(parsed);
+  release(parsed);
+  free(buf);
+  return 0;
 }
