720f05b6c8f4afe87aaff6d51b1bcb9c351d07ff
Stefano Frontini <stefanofrontini75@gmail.com>
2026-04-10 14:47:22 +0200

    cs2

--- antirez/chord-shunting-yard/chord-shunting-yard.c
+++ antirez/chord-shunting-yard/chord-shunting-yard.c
@@ -7,6 +7,9 @@
 #include <string.h>
 #include <unistd.h>
 
+#define SY_OK 0
+#define SY_ERR 1
+
 /* ------------------------ Allocation wrappers ----------------------------*/
 
 void *xmalloc(size_t size) {
@@ -26,206 +29,615 @@ void *xrealloc(void *old_ptr, size_t size) {
   }
   return ptr;
 }
+
 /* ----------------   Data structures: Lexer ------------------ */
 
-typedef enum {LYRIC = 0, CHORD, ENDOFLINE, ENDOFFILE, ILLEGAL } TokenType;
+typedef enum {
+  LYRIC_TOKEN = 0,
+  CHORD_TOKEN,
+  ENDOFLINE,
+  ENDOFFILE,
+  WORD_MULT,
+  ILLEGAL,
+  LIST_TOKEN
+} TokenType;
 
 typedef struct Token {
-    int refcount;
-    TokenType type;
-    union {
-       char symbol;
-       struct {
-          char *ptr;
-          size_t len;
-       } str;
-    };
+  int refcount;
+  TokenType type;
+  union {
+    char symbol;
+    struct {
+      char *ptr;
+      size_t len;
+    } str;
+    struct {
+      struct Token **ele;
+      size_t len;
+    } list;
+  };
 } Token;
 
 typedef struct Lexer {
-    char *prg; // Program to parse
-    char *p; // Next token to parse
-    Token *curToken;
+  char *prg; // Program to parse
+  char *p;   // Next token to parse
+  Token *curToken;
 
 } Lexer;
 
 /* ------------------  Token related functions ----------------  */
 
-void retainToken(Token *t){
-    t->refcount++;
+void retainToken(Token *t) {
+  t->refcount++;
 }
-void releaseToken(Token *t){
-    assert(t->refcount > 0);
-    t->refcount--;
-    if(t->refcount == 0){
-        switch(t->type){
-            case LYRIC:
-            case CHORD:
-              free(t->str.ptr);
-              break;
-            default:
-              break;
-        }
-        free(t);
+void releaseToken(Token *t) {
+  assert(t->refcount > 0);
+  t->refcount--;
+  if (t->refcount == 0) {
+    switch (t->type) {
+      case LYRIC_TOKEN:
+      case CHORD_TOKEN:
+        free(t->str.ptr);
+        break;
+      default:
+        break;
     }
+    free(t);
+  }
 }
 
 /* Return true if the character 'c' is one of the characters acceptable for the
  * lyric token. */
-int isStringConstant(Lexer *l){
-    char c = l->p[0];
-    char symbols_char[] = "[]\n\r";
-    return isascii(c) && strchr(symbols_char, c) == NULL;
+int isStringConstant(Lexer *l) {
+  char c = l->p[0];
+  char symbols_char[] = "[]\n\r";
+  return isascii(c) && strchr(symbols_char, c) == NULL;
 }
 
-
-void readLyric(Lexer *l){
-    char *start = l->p;
-    while(isStringConstant(l)){
-        l->p++;
-    }
-    char *end = l->p;
-    size_t len = end - start;
-
-    Token *t = xmalloc(sizeof(Token));
-    char *str = xmalloc(len + 1);
-    t->refcount = 1;
-    t->type = LYRIC;
-    t->str.ptr = str;
-    t->str.len = len;
-    memcpy(t->str.ptr, start, len);
-    t->str.ptr[len] = 0;
-    l->curToken = t;
-}
-
-int isBeginningChord(Lexer *l){
-    return l->p[0] == '[';
-}
-
-void readChord(Lexer *l){
+void readLyric(Lexer *l) {
+  char *start = l->p;
+  while (isStringConstant(l)) {
     l->p++;
-    if(!isStringConstant(l)){
-        fprintf(stderr, "No string after open paren\n");
-        exit(1);
-    }
-    char *start = l->p;
-    while(isStringConstant(l)){
-        l->p++;
-    }
-    char *end = l->p;
-    size_t len = end - start;
-    if(l->p[0] != ']'){
-        printf("char: %c\n", l->p[0]);
-        fprintf(stderr, "Unmatched close paren\n");
-        exit(1);
-    }
+  }
+  char *end = l->p;
+  size_t len = end - start;
+
+  Token *t = xmalloc(sizeof(Token));
+  char *str = xmalloc(len + 1);
+  t->refcount = 1;
+  t->type = LYRIC_TOKEN;
+  t->str.ptr = str;
+  t->str.len = len;
+  memcpy(t->str.ptr, start, len);
+  t->str.ptr[len] = 0;
+  l->curToken = t;
+}
+
+int isBeginningChord(Lexer *l) {
+  return l->p[0] == '[';
+}
+
+void readChord(Lexer *l) {
+  l->p++;
+  if (!isStringConstant(l)) {
+    fprintf(stderr, "No string after open paren\n");
+    exit(1);
+  }
+  char *start = l->p;
+  while (isStringConstant(l)) {
     l->p++;
+  }
+  char *end = l->p;
+  size_t len = end - start;
+  if (l->p[0] != ']') {
+    printf("char: %c\n", l->p[0]);
+    fprintf(stderr, "Unmatched close paren\n");
+    exit(1);
+  }
+  l->p++;
 
-    Token *t = xmalloc(sizeof(Token));
-    char *str = xmalloc(len + 1);
-    t->refcount = 1;
-    t->type = CHORD;
-    t->str.ptr = str;
-    t->str.len = len;
-    memcpy(t->str.ptr, start, len);
-    t->str.ptr[len] = 0;
-    l->curToken = t;
-
+  Token *t = xmalloc(sizeof(Token));
+  char *str = xmalloc(len + 1);
+  t->refcount = 1;
+  t->type = CHORD_TOKEN;
+  t->str.ptr = str;
+  t->str.len = len;
+  memcpy(t->str.ptr, start, len);
+  t->str.ptr[len] = 0;
+  l->curToken = t;
+}
+Token *createEmptyChordToken(void) {
+  Token *t = xmalloc(sizeof(Token));
+  t->type = CHORD_TOKEN;
+  t->refcount = 1;
+  t->str.ptr = "emptyChord";
+  t->str.len = 10;
+  return t;
 }
 
-void readEndOfLine(Lexer *l, char s){
-    Token *t = xmalloc(sizeof(Token));
-    t->refcount = 1;
-    t->type = ENDOFLINE;
-    t->symbol = s;
-    l->curToken = t;
-    l->p++;
+void readEndOfLine(Lexer *l, char s) {
+  Token *t = xmalloc(sizeof(Token));
+  t->refcount = 1;
+  t->type = ENDOFLINE;
+  t->symbol = s;
+  l->curToken = t;
+  l->p++;
 }
-void readIllegalChar(Lexer *l, char s){
-    Token *t = xmalloc(sizeof(Token));
-    t->refcount = 1;
-    t->type = ILLEGAL;
-    t->symbol = s;
-    l->curToken = t;
-    l->p++;
-}
-
-void readEndOfFile(Lexer *l){
-    Token *t = xmalloc(sizeof(Token));
-    t->refcount = 1;
-    t->type = ENDOFFILE;
-    t->symbol = 0;
-    l->curToken = t;
+void readIllegalChar(Lexer *l, char s) {
+  Token *t = xmalloc(sizeof(Token));
+  t->refcount = 1;
+  t->type = ILLEGAL;
+  t->symbol = s;
+  l->curToken = t;
+  l->p++;
 }
 
-void advanceLexer(Lexer *l){
-      releaseToken(l->curToken);
-      char c = l->p[0];
-      if(isStringConstant(l)){
-        readLyric(l);
-      } else if(isBeginningChord(l)){
-        readChord(l);
-      } else if(c == '\n' || c == '\r'){
-        readEndOfLine(l, c);
+void readEndOfFile(Lexer *l) {
+  Token *t = xmalloc(sizeof(Token));
+  t->refcount = 1;
+  t->type = ENDOFFILE;
+  t->symbol = 0;
+  l->curToken = t;
+}
+
+Token *createTokenList(void) {
+  Token *t = xmalloc(sizeof(Token));
+  t->type = LIST_TOKEN;
+  t->refcount = 1;
+  t->list.ele = NULL;
+  t->list.len = 0;
+  return t;
+}
+
+Token *createTokenWordMult(void) {
+  Token *t = xmalloc(sizeof(Token));
+  t->type = WORD_MULT;
+  t->refcount = 1;
+  t->symbol = WORD_MULT;
+  return t;
+}
+
+Token *createEmptyLyricToken(void) {
+  Token *t = xmalloc(sizeof(Token));
+  t->type = LYRIC_TOKEN;
+  t->refcount = 1;
+  t->str.ptr = "emptyLyric";
+  t->str.len = 10;
+  return t;
+}
+
+void advanceLexer(Lexer *l) {
+  releaseToken(l->curToken);
+  char c = l->p[0];
+  if (isStringConstant(l)) {
+    readLyric(l);
+  } else if (isBeginningChord(l)) {
+    readChord(l);
+  } else if (c == '\n' || c == '\r') {
+    readEndOfLine(l, c);
+  } else if (c == 0) {
+    readEndOfFile(l);
+  } else {
+    readIllegalChar(l, c);
+  }
+}
+
+void printToken(Token *t) {
+  switch (t->type) {
+    case LYRIC_TOKEN:
+    case CHORD_TOKEN:
+      printf("Current token is: %s\n", t->str.ptr);
+      break;
+    case ILLEGAL:
+      printf("Current token is: %c\n", t->symbol);
+      break;
+    case ENDOFLINE:
+      printf("Current token is: 'newLine'\n");
+      break;
+    case WORD_MULT:
+      printf("Current token is: *\n");
+      break;
+    case LIST_TOKEN:
+      for (size_t i = 0; i < t->list.len; i++) {
+        Token *el = t->list.ele[i];
+        printToken(el);
+        if (i != t->list.len - 1) {
+          printf(" ");
+        }
       }
-      else if(c == 0) {
-        readEndOfFile(l);
-      } else {
-        readIllegalChar(l, c);
-      }
+      break;
+
+    default:
+      printf("? - %d\n", t->type);
+      break;
+  }
 }
 
-void printToken(Token *t){
-    switch(t->type){
-        case LYRIC:
-        case CHORD:
-           printf("Current token is: %s\n", t->str.ptr);
-           break;
-        case ILLEGAL:
-           printf("Current token is: %c\n", t->symbol);
-           break;
-        case ENDOFLINE:
-           printf("Current token is: 'newLine'\n");
-           break;
+/* --------------   Data structures: Chord Sheet Object ------------------- */
 
-        default:
+typedef enum { SONG = 0, LINE, WORD, CHORD, LYRIC, LIST } CsObjType;
+
+typedef struct CsObj {
+  int refcount;
+  CsObjType type;
+  union {
+    struct {
+      struct CsObj **ele;
+      size_t len;
+    } list;
+    struct {
+      char *ptr;
+      size_t len;
+
+    } str;
+  };
+} CsObj;
+
+/* -----------   CsObj related functions ------------ */
+
+/* Allocate and initialize e new Chord Sheet Object */
+
+CsObj *createObject(CsObjType type) {
+  CsObj *o = xmalloc(sizeof(CsObj));
+  o->refcount = 1;
+  o->type = type;
+  return o;
+}
+
+CsObj *createSongObject(void) {
+  CsObj *o = createObject(SONG);
+  o->list.ele = NULL;
+  o->list.len = 0;
+  return o;
+}
+
+CsObj *createLineObject(void) {
+  CsObj *o = createObject(LINE);
+  o->list.ele = NULL;
+  o->list.len = 0;
+  return o;
+}
+
+CsObj *createWordObject(void) {
+  CsObj *o = createObject(WORD);
+  o->list.ele = NULL;
+  o->list.len = 0;
+  return o;
+}
+
+CsObj *createChordObject(char *str, size_t len) {
+  CsObj *o = createObject(CHORD);
+  if (len > 0) {
+    o->str.ptr = xmalloc(len + 1);
+    memcpy(o->str.ptr, str, len);
+    o->str.ptr[len] = 0;
+  } else {
+    o->str.ptr = str;
+  }
+  o->str.len = len;
+  return o;
+}
+
+CsObj *createLyricObject(char *str, size_t len) {
+  CsObj *o = createObject(LYRIC);
+  if (len > 0) {
+    o->str.ptr = xmalloc(len + 1);
+    memcpy(o->str.ptr, str, len);
+    o->str.ptr[len] = 0;
+  } else {
+    o->str.ptr = str;
+  }
+  o->str.len = len;
+  return o;
+}
+
+CsObj *createListObject(void) {
+  CsObj *o = createObject(LIST);
+  o->list.ele = NULL;
+  o->list.len = 0;
+  return o;
+}
+
+/* Add the new element at the end of the list. It is up to the caller to
+ * increment the reference count to the list. */
+void listPush(Token *l, Token *ele) {
+  l->list.ele = xrealloc(l->list.ele, sizeof(Token *) * (l->list.len + 1));
+  l->list.ele[l->list.len] = ele;
+  l->list.len++;
+}
+
+Token *listPop(Token *l) {
+  if (l->list.len > 0) {
+    Token *t = l->list.ele[l->list.len - 1];
+    l->list.len--;
+    return t;
+  } else {
+    return NULL;
+  }
+}
+
+void retainCsObj(CsObj *o) {
+  o->refcount++;
+}
+
+void releaseCsObj(CsObj *o) {
+  assert(o->refcount > 0);
+  o->refcount--;
+  if (o->refcount == 0) {
+    switch (o->type) {
+      case SONG:
+      case LINE:
+      case WORD:
+        for (size_t i = 0; i < o->list.len; i++) {
+          releaseCsObj(o->list.ele[i]);
+        }
+        free(o->list.ele);
+        break;
+      case CHORD:
+      case LYRIC:
+        free(o->str.ptr);
+        break;
+      default:
         break;
     }
+    free(o);
+  }
 }
+
+void printCsObj(CsObj *o) {
+  switch (o->type) {
+    case SONG:
+    case LINE:
+    case WORD:
+      for (size_t i = 0; i < o->list.len; i++) {
+        if (o->list.len == 0) {
+          printf("");
+        } else {
+          printCsObj(o->list.ele[i]);
+        }
+      }
+      if (o->type == LINE) {
+        printf("\n");
+      }
+      break;
+    case CHORD:
+      if (o->str.len == 0) {
+        printf("");
+      } else {
+        printf("[%s](type: %d)", o->str.ptr, o->type);
+      }
+      break;
+    case LYRIC:
+      if (o->str.len == 0) {
+        printf("");
+      } else {
+        printf("%s(type: %d)", o->str.ptr, o->type);
+      }
+      break;
+    default:
+      break;
+  }
+}
+
+typedef struct precedenceTableEntry {
+  char s;
+  int i;
+} pTableEntry;
+
+typedef struct precedenceTable {
+  pTableEntry **pTable;
+  int count;
+} pTable;
+
+struct SyCtx;
+typedef int (*MathFn)(struct SyCtx *ctx, char s);
+
+typedef struct symbolTableEntry {
+  char s;
+  MathFn f;
+} sTableEntry;
+
+typedef struct symbolTable {
+  sTableEntry **sTable;
+  int count;
+} sTable;
+
+typedef struct SyCtx {
+  CsObj *stack;
+  Token *opStack;
+  Token *queue;
+  pTable precedenceTable;
+  sTable symbolTable;
+} SyCtx;
+
+void addSymbolPrecedence(SyCtx *ctx, char s, int i) {
+  ctx->precedenceTable.pTable =
+      xrealloc(ctx->precedenceTable.pTable,
+               sizeof(pTableEntry *) * (ctx->precedenceTable.count + 1));
+  pTableEntry *entry = xmalloc(sizeof(pTableEntry));
+  entry->s = s;
+  entry->i = i;
+  ctx->precedenceTable.pTable[ctx->precedenceTable.count] = entry;
+  ctx->precedenceTable.count++;
+}
+
+SyCtx *createContext(void) {
+  SyCtx *ctx = xmalloc(sizeof(SyCtx));
+  ctx->stack = createListObject();
+  ctx->opStack = createTokenList();
+  ctx->queue = createTokenList();
+  ctx->precedenceTable.pTable = NULL;
+  ctx->precedenceTable.count = 0;
+  addSymbolPrecedence(ctx, CHORD_TOKEN, 3);
+  addSymbolPrecedence(ctx, WORD_MULT, 2);
+  addSymbolPrecedence(ctx, ENDOFLINE, 1);
+
+  ctx->symbolTable.sTable = NULL;
+  ctx->symbolTable.count = 0;
+  //   addSymbolFn(ctx, '+', basicMathFunctions);
+  //   addSymbolFn(ctx, '-', basicMathFunctions);
+  //   addSymbolFn(ctx, '*', basicMathFunctions);
+  //   addSymbolFn(ctx, '/', basicMathFunctions);
+  //   addSymbolFn(ctx, '~', basicMathFunctions);
+
+  return ctx;
+}
+
+int getPrecedence(SyCtx *ctx, Token *t) {
+  // printf("Token type, symbol: %d, %c\n", t->type, t->symbol);
+  char s = t->type;
+  for (int i = 0; i < ctx->precedenceTable.count; i++) {
+    if (ctx->precedenceTable.pTable[i]->s == s) {
+      return ctx->precedenceTable.pTable[i]->i;
+    }
+  }
+  printf("Error: %c\n", s);
+  return -1;
+}
+
+void ctxEnqueue(SyCtx *ctx, Token *t);
+
+/* Push the token on the context operator stack. It's up the caller to increment
+ * the refcount. */
+void ctxOpStackPush(SyCtx *ctx, Token *t) {
+
+  //   if (o->symbol == '(') {
+  //     listPush(ctx->stack, o);
+  //     return;
+  //   }
+
+  //   if (o->symbol == ')') {
+  //     while (ctx->stack->list.len > 0) {
+  //       SyObj *popped = listPop(ctx->stack);
+
+  //       if (popped->symbol == '(') {
+  //         release(popped);
+  //         break;
+  //       }
+
+  //       ctxEnqueue(ctx, popped);
+  //     }
+
+  //     release(o);
+  //     return;
+  //   }
+
+  while (ctx->opStack->list.len > 0) {
+    Token *peek = ctx->opStack->list.ele[ctx->opStack->list.len - 1];
+
+    // if (peek->symbol == '(') {
+    //   break;
+    // }
+
+    int precPeek = getPrecedence(ctx, peek);
+    int precCurrent = getPrecedence(ctx, t);
+
+    /* ASSOCIATIVITY RULE:
+      If current operator is right associative ('~'), we use >
+      else (left associative), we use >=
+    */
+    int shouldPop = 0;
+    if (t->type == CHORD_TOKEN) {
+      shouldPop = (precPeek > precCurrent);
+    } else {
+      shouldPop = (precPeek >= precCurrent);
+    }
+
+    if (shouldPop) {
+      Token *popped = listPop(ctx->opStack);
+      ctxEnqueue(ctx, popped);
+    } else {
+      break;
+    }
+  }
+
+  listPush(ctx->opStack, t);
+}
+
+/* Enqueue the token on the context queue. It's up the caller to increment the
+ * refcount. */
+void ctxEnqueue(SyCtx *ctx, Token *t) {
+  listPush(ctx->queue, t);
+};
 
 /* -------------------------  Main ----------------------------- */
 
-int main(int argc, char **argv){
-    if(argc !=2){
-        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
-        return 1;
-    }
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+    return 1;
+  }
 
-    FILE *fp = fopen(argv[1], "r");
+  FILE *fp = fopen(argv[1], "r");
 
   if (fp == NULL) {
     fprintf(stderr, "Unable to open the file\n");
     return 1;
   }
 
-    fseek(fp, 0, SEEK_END);
-    long file_size = ftell(fp);
-    fseek(fp, 0, SEEK_SET);
-    char *buf = xmalloc(file_size + 1);
-    fread(buf, file_size, 1, fp);
-    buf[file_size] = 0;
-    printf("text is: %s\n", buf);
-    fclose(fp);
-    Lexer l;
-    l.prg = buf;
-    l.p = buf;
-    readEndOfFile(&l);
-    advanceLexer(&l);
-    while(l.curToken->type != ENDOFFILE){
-        printToken(l.curToken);
-        advanceLexer(&l);
-    }
-    free(buf);
-    return 0;
-}
+  fseek(fp, 0, SEEK_END);
+  long file_size = ftell(fp);
+  fseek(fp, 0, SEEK_SET);
+  char *buf = xmalloc(file_size + 1);
+  fread(buf, file_size, 1, fp);
+  buf[file_size] = 0;
+  printf("text is: %s\n", buf);
+  fclose(fp);
+  Lexer l;
+  l.prg = buf;
+  l.p = buf;
+  readEndOfFile(&l);
+  advanceLexer(&l);
+  SyCtx *ctx = createContext();
 
+  while (l.curToken->type != ENDOFFILE) {
+    switch (l.curToken->type) {
+      case LYRIC_TOKEN:
+        retainToken(l.curToken); // queue is co-owner
+        Token *emptyChord = createEmptyChordToken();
+        ctxOpStackPush(ctx, emptyChord);
+        ctxEnqueue(ctx, l.curToken);
+        advanceLexer(&l);
+        if(l.curToken->type == CHORD_TOKEN || l.curToken->type == LYRIC_TOKEN){
+          Token *w = createTokenWordMult();
+          ctxOpStackPush(ctx, w);
+        }
+        break;
+      case CHORD_TOKEN:
+        retainToken(l.curToken); // opStack is co-owner
+        ctxOpStackPush(ctx, l.curToken);
+        advanceLexer(&l);
+        if(l.curToken->type == LYRIC_TOKEN){
+          retainToken(l.curToken); // opStack is co-owner
+          ctxEnqueue(ctx, l.curToken);
+          advanceLexer(&l);
+        } else {
+          Token *emptyLyric = createEmptyLyricToken();
+          ctxEnqueue(ctx, emptyLyric);
+        }
+        if(l.curToken->type == CHORD_TOKEN || l.curToken->type == LYRIC_TOKEN){
+          Token *w = createTokenWordMult();
+          ctxOpStackPush(ctx, w);
+        }
+        break;
+      case ENDOFLINE:
+        retainToken(l.curToken); // opStack is co-owner
+        ctxOpStackPush(ctx, l.curToken);
+        advanceLexer(&l);
+        break;
+
+      default:
+        printf("?\n");
+        break;
+    }
+  }
+
+  while (ctx->opStack->list.len > 0) {
+    Token *popped = listPop(ctx->opStack);
+    ctxEnqueue(ctx, popped);
+  }
+  printf("Queue is: \n");
+  printToken(ctx->queue);
+  free(buf);
+  return 0;
+}
