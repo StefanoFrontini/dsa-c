8db238a309500371efc2067e7f70ba03047c9a60
Stefano Frontini <stefanofrontini75@gmail.com>
2026-05-02 22:17:49 +0200

    cp3

--- antirez/chord-pratt/chord-pratt.c
+++ antirez/chord-pratt/chord-pratt.c
@@ -53,11 +53,65 @@ typedef struct Lexer {
   char *p;
   Token curToken;
   int isNextTokenEmptyString;
+  int isNextTokenWordMult;
 } Lexer;
+
+
+// AST Object (PObj)
+
+typedef enum { LYRIC = 0, CHORD, INFIX, WORD, LINE, SONG } PObjType;
+
+typedef struct PObj {
+  int refcount;
+  PObjType type;
+  union {
+    int i;
+    struct {
+      struct PObj *left;
+      char operator;
+      struct PObj *right;
+    } infix;
+    struct {
+      struct PObj *chord;
+      struct PObj *lyric;
+    } word;
+    struct {
+      struct PObj **ele;
+      size_t len;
+    } list;
+  };
+} PObj;
 
 typedef struct CpCtx {
   Lexer lexer;
+  PObj *ast;
 } CpCtx;
+
+/* Prototypes for the parsing table and related functions */
+
+typedef PObj *(*PrefixFn)(CpCtx *ctx);
+typedef PObj *(*InfixFn)(CpCtx *ctx, PObj *left);
+
+typedef struct {
+  PrefixFn prefixFn;
+  InfixFn infixFn;
+  int precedence;
+} pTableEntry;
+
+PObj *parsePrefix(CpCtx *ctx);
+PObj *parseInfix(CpCtx *ctx, PObj *left);
+PObj *parseExpression(CpCtx *ctx, int precedence);
+
+/* ------------------ Static Parsing Table ---------------- */
+
+static pTableEntry rulesTable[] = {
+    [TOKEN_CHORD] = {parsePrefix, NULL, 0},
+    [TOKEN_LYRIC] = {parsePrefix, NULL, 0},
+    [TOKEN_ENDOFLINE] = {NULL, parseInfix, 2},
+    [TOKEN_WORD_MULT] = {NULL, parseInfix, 3},
+    [TOKEN_ENDOFFILE] = {NULL, NULL, 0},
+};
+
 
 int isStringConstant(CpCtx *ctx) {
   char c = ctx->lexer.p[0];
@@ -66,6 +120,12 @@ int isStringConstant(CpCtx *ctx) {
 }
 
 void readLyric(CpCtx *ctx) {
+  if (ctx->lexer.curToken.type != TOKEN_CHORD) {
+    ctx->lexer.curToken.type = TOKEN_CHORD;
+    ctx->lexer.curToken.str.ptr = NULL;
+    ctx->lexer.curToken.str.len = 0;
+    return;
+  }
   char *start = ctx->lexer.p;
   while (isStringConstant(ctx)) {
     ctx->lexer.p++;
@@ -78,6 +138,9 @@ void readLyric(CpCtx *ctx) {
   ctx->lexer.curToken.str.len = len;
   memcpy(ctx->lexer.curToken.str.ptr, start, len);
   ctx->lexer.curToken.str.ptr[len] = 0;
+  if (ctx->lexer.p[0] == '[') {
+    ctx->lexer.isNextTokenWordMult = 1;
+  }
 }
 
 int isBeginningChord(CpCtx *ctx) {
@@ -138,6 +201,10 @@ void advanceLexer(CpCtx *ctx) {
     ctx->lexer.curToken.str.ptr = NULL;
     ctx->lexer.curToken.str.len = 0;
     ctx->lexer.isNextTokenEmptyString = 0;
+  } else if (ctx->lexer.isNextTokenWordMult) {
+    ctx->lexer.curToken.type = TOKEN_WORD_MULT;
+    ctx->lexer.curToken.symbol = '*';
+    ctx->lexer.isNextTokenWordMult = 0;
   } else {
     char c = ctx->lexer.p[0];
     if (isStringConstant(ctx)) {
@@ -164,7 +231,14 @@ void printToken(Token *t) {
       }
       break;
     case TOKEN_CHORD:
-      printf("Current token is Chord: %s\n", t->str.ptr);
+      if (t->str.ptr == NULL) {
+        printf("Current token is Empty Chord: \n");
+      } else {
+        printf("Current token is Chord: %s\n", t->str.ptr);
+      }
+      break;
+    case TOKEN_WORD_MULT:
+      printf("Current token is WORD_MULT: %c\n", t->symbol);
       break;
     case TOKEN_ILLEGAL:
       printf("Current token is Illegal: %c\n", t->symbol);
@@ -187,8 +261,134 @@ void initContext(CpCtx *ctx, char *buf) {
   ctx->lexer.prg = buf;
   ctx->lexer.p = buf;
   ctx->lexer.isNextTokenEmptyString = 0;
-
+  ctx->lexer.isNextTokenWordMult = 0;
 }
+
+/* ------------------  AST Object Management (PObj) ----------------  */
+
+void release(PObj *o);
+
+/* Free an object and all the other nested objects. */
+void freeObject(PObj *o) {
+  switch (o->type) {
+    case INFIX:
+      release(o->infix.left);
+      release(o->infix.right);
+      break;
+    case WORD:
+      break;
+    case NEGATION:
+      release(o->neg.ele);
+      break;
+    default:
+      break;
+  }
+  free(o);
+}
+
+void retain(PObj *o) {
+  o->refcount++;
+}
+
+void release(PObj *o) {
+  if (!o) return;
+  assert(o->refcount > 0);
+  o->refcount--;
+  if (o->refcount == 0) freeObject(o);
+}
+
+PObj *createObject(PObjType type) {
+  PObj *o = xmalloc(sizeof(PObj));
+  o->refcount = 1;
+  o->type = type;
+  return o;
+}
+
+PObj *createLyricObject(int n) {
+  PObj *o = createObject();
+  o->i = n;
+  return o;
+}
+
+PObj *createInfixObject(PObj *left, char operator, PObj *right) {
+  PObj *o = createObject(INFIX);
+  o->infix.left = left;
+  o->infix.operator = operator;
+  o->infix.right = right;
+  return o;
+}
+
+PObj *createNegationObject(PObj *ele) {
+  PObj *o = createObject(NEGATION);
+  o->neg.ele = ele;
+  return o;
+}
+
+
+/* ------------------  Pratt Parser logic ----------------  */
+
+int getPrecedence(CpCtx *ctx) {
+  return rulesTable[ctx->lexer.curToken.type].precedence;
+}
+
+PObj *parsePrefix(CpCtx *ctx) {
+  if (ctx->lexer.curToken.type == TOKEN_LYRIC) {
+    PObj *o = createNumberObject(ctx->lexer.curToken.i);
+    advanceLexer(ctx);
+    return o;
+  } else if (ctx->lexer.curToken.type == TOKEN_MINUS) {
+    advanceLexer(ctx);
+    PObj *ele = parseExpression(ctx, PREFIX_PRECEDENCE);
+    return createNegationObject(ele);
+  } else if (ctx->lexer.curToken.type == TOKEN_OPENPAREN) {
+    advanceLexer(ctx);
+    PObj *innerAST = parseExpression(ctx, 0);
+
+    if (ctx->lexer.curToken.type != TOKEN_CLOSEPAREN) {
+      printf("Syntax error: close paren expected ')'\n");
+      exit(1);
+    }
+    advanceLexer(ctx);
+    return innerAST;
+  }
+  printf("Error: No prefix function for the token %d\n",
+         ctx->lexer.curToken.type);
+  return NULL;
+}
+
+PObj *parseInfix(CpCtx *ctx, PObj *left) {
+  int precedence = getPrecedence(ctx);
+  char op = ctx->lexer.curToken.symbol;
+  advanceLexer(ctx);
+  PObj *right = parseExpression(ctx, precedence);
+  return createInfixObject(left, op, right);
+}
+
+PObj *parseExpression(CpCtx *ctx, int precedence) {
+  TokenType type = ctx->lexer.curToken.type;
+  PrefixFn prefix = rulesTable[type].prefixFn;
+
+  if (prefix == NULL) {
+    printf("Error: Token %d cannot be used in prefix notation \n", type);
+    return NULL;
+  }
+
+  PObj *left = prefix(ctx);
+
+  while (ctx->lexer.curToken.type != TOKEN_ENDOFFILE &&
+         precedence < getPrecedence(ctx)) {
+
+    InfixFn infix = rulesTable[ctx->lexer.curToken.type].infixFn;
+
+    if (infix == NULL) {
+      return left;
+    }
+
+    left = infix(ctx, left);
+  }
+  return left;
+}
+
 
 /* -------------------------  Main ----------------------------- */
 
