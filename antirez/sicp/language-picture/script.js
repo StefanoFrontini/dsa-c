1c7ccef8d54297bc704502b8df07170553e09581
Stefano Frontini <stefanofrontini75@gmail.com>
2026-08-18 17:42:05 +0200

    client_http_54

--- antirez/sicp/language-picture/script.js
+++ antirez/sicp/language-picture/script.js
@@ -30,7 +30,7 @@ const z = pair(x, y);
 // console.log(tail(x));
 // console.log(head(head(z)));
 function list(...args) {
-    console.log("args: ", args);
+    // console.log("args: ", args);
     function list_rec(acc, ...args_rec) {
         if (args_rec.length === 0)
             return acc;
@@ -41,6 +41,10 @@ function list(...args) {
     return list_rec(null, ...args);
 }
 function print_list(l) {
+    if (typeof l === "number" || l === null) {
+        console.log(l);
+        return;
+    }
     function print_list_rec(l, acc) {
         if (l === null)
             return acc;
@@ -49,8 +53,42 @@ function print_list(l) {
     const res = print_list_rec(l, []);
     console.log(res);
 }
-const l = list(1, list(4, 5), 3);
-// const l = list(1, 2, 3, 4);
-console.log("l:", l);
-console.log("print_list");
-print_list(l);
+// const l = list(1, list(4, 5), 3);
+const l = list(1, 2, 3, 4);
+const l2 = list(5, 6, 7, 8);
+// console.log("l:", l);
+// console.log("print_list");
+// const p = pair(10, l);
+// print_list(l);
+// print_list(head(p));
+// print_list(tail(p));
+function list_ref(items, n) {
+    return n === 0 ? head(items) : list_ref(tail(items), n - 1);
+}
+function is_null(l) {
+    return l === null;
+}
+function len(items) {
+    return is_null(items) ? 0 : 1 + len(tail(items));
+}
+function append(list1, list2) {
+    return is_null(list1) ? list2 : pair(head(list1), append(tail(list1), list2));
+}
+// print_list(len(l));
+// print_list(append(l, l2));
+function map(fun, items) {
+    return is_null(items)
+        ? null
+        : pair(fun(head(items)), map(fun, tail(items)));
+}
+function for_each(fun, items) {
+    if (is_null(items))
+        return;
+    fun(head(items));
+    for_each(fun, tail(items));
+}
+const a = pair(list(1, 2), list(3, 4));
+print_list(len(list(a, a)));
+// for_each(print_list, list(10, 20, 30));
+// for_each((x:number) => x * 2, l);
+// print_list(map(((x:number) => x * x), l));
--- antirez/sicp/language-picture/script.ts
+++ antirez/sicp/language-picture/script.ts
@@ -50,16 +50,16 @@ function list(...args: (Pair | number | null)[]) {
     if (args_rec.length === 0) return acc;
     // console.log("args_rec[0]: ", args_rec[0]);
     // console.log("...args_rec.slice(1):", ...args_rec.slice(1));
-    return pair(
-      args_rec[0],
-      list_rec(acc, ...args_rec.slice(1)),
-    );
-
+    return pair(args_rec[0], list_rec(acc, ...args_rec.slice(1)));
   }
   return list_rec(null, ...args);
 }
 
 function print_list(l: Pair | number | null) {
+  if (typeof l === "number" || l === null) {
+    console.log(l);
+    return;
+  }
   function print_list_rec(
     l: Pair | number | null,
     acc: (Pair | number | null)[],
@@ -71,8 +71,61 @@ function print_list(l: Pair | number | null) {
   console.log(res);
 }
 
-const l = list(1, list(4, 5), 3);
-// const l = list(1, 2, 3, 4);
-console.log("l:", l);
-console.log("print_list");
-print_list(l);
+// const l = list(1, list(4, 5), 3);
+const l = list(1, 2, 3, 4);
+const l2 = list(5, 6, 7, 8);
+// console.log("l:", l);
+// console.log("print_list");
+// const p = pair(10, l);
+// print_list(l);
+// print_list(head(p));
+// print_list(tail(p));
+function list_ref(
+  items: Pair | number | null,
+  n: number,
+): Pair | number | null {
+  return n === 0 ? head(items) : list_ref(tail(items), n - 1);
+}
+
+function is_null(l: Pair | number | null) {
+  return l === null;
+}
+
+function len(items: Pair | number | null): number {
+  return is_null(items) ? 0 : 1 + len(tail(items));
+}
+
+function append(
+  list1: Pair | number | null,
+  list2: Pair | number | null,
+): Pair | number | null {
+  return is_null(list1) ? list2 : pair(head(list1), append(tail(list1), list2));
+}
+// print_list(len(l));
+// print_list(append(l, l2));
+
+function map<T extends number | Pair | null>(
+  fun: (a: T) => T,
+  items: Pair | number | null,
+): Pair | number | null {
+  return is_null(items)
+    ? null
+    : pair(fun(head(items) as T), map(fun, tail(items)));
+}
+
+function for_each<T extends number | Pair | null>(
+  fun: (a: T) => void,
+  items: Pair | number | null,
+): void {
+  if (is_null(items)) return;
+  fun(head(items) as T);
+  for_each(fun, tail(items));
+}
+
+const a = pair(list(1, 2), list(3, 4))
+
+print_list(len(list(a, a)));
+
+// for_each(print_list, list(10, 20, 30));
+// for_each((x:number) => x * 2, l);
+// print_list(map(((x:number) => x * x), l));
