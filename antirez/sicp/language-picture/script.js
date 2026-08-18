2025aa7ecedc7dfc1e4208fae101a0a1c934b3fc
Stefano Frontini <stefanofrontini75@gmail.com>
2026-08-18 11:15:15 +0200

    client_http_52

--- antirez/sicp/language-picture/script.js
+++ antirez/sicp/language-picture/script.js
@@ -8,11 +8,67 @@ if (!ctx)
 console.log(ctx);
 const CANVAS_WIDTH = (canvas.width = 600);
 const CANVAS_HEIGHT = (canvas.height = 600);
-// type PairFn = (a: number | Pair, b: number | Pair) => Pair;
-// const pair: PairFn = (a, b) => ({ head: a, tail: b });
+// type List = {
+//   head: number | null;
+//   tail: List | null;
+// }
+// cons
 function pair(a, b) {
     return {
         head: a,
         tail: b,
     };
 }
+// car
+function head(p) {
+    return typeof p === "number" || p === null ? p : p.head;
+}
+// cdr
+function tail(p) {
+    return typeof p === "number" || p === null ? p : p.tail;
+}
+const x = pair(1, 2);
+const y = pair(3, 4);
+const z = pair(x, y);
+console.log(head(x));
+console.log(tail(x));
+console.log(head(head(z)));
+function list(...args) {
+    console.log("args: ", args);
+    function list_rec(acc, ...args_rec) {
+        if (args_rec.length === 0)
+            return acc;
+        console.log("args_rec[0]: ", args_rec[0]);
+        console.log("...args_rec.slice(1):", ...args_rec.slice(1));
+        return pair(args_rec[0], 
+        // list_rec(acc, ...args_rec.slice(0, 1)),
+        list_rec(acc, ...args_rec.slice(1)));
+        // else {
+        //   return pair(
+        //     list_rec(acc, ...args_rec[0]),
+        //     list_rec(acc, ...args_rec.slice(1)),
+        //   );
+        // }
+        // return args_rec.length === 0
+        //   ? acc
+        //   : pair(args_rec[0], list_rec(acc, ...args_rec.slice(1)));
+    }
+    return list_rec(null, ...args);
+}
+function print_list(l) {
+    function print_list_rec(l, acc) {
+        if (l === null)
+            return acc;
+        const h = head(l);
+        if (typeof h === "number") {
+            return print_list_rec(tail(l), [...acc, h]);
+        }
+    }
+    const res = print_list_rec(l, []);
+    console.log(res);
+}
+const l = list(1, list(4, 5), 3);
+// const l = list(1, 2, 3, 4);
+console.log("l:", l);
+console.log("print_list");
+print_list(l);
--- antirez/sicp/language-picture/script.ts
+++ antirez/sicp/language-picture/script.ts
@@ -10,17 +10,84 @@ const CANVAS_WIDTH = (canvas.width = 600);
 const CANVAS_HEIGHT = (canvas.height = 600);
 
 type Pair = {
-  head: number | Pair;
-  tail: number | Pair;
+  head: number | Pair | null;
+  tail: number | Pair | null;
 };
+// type List = {
+//   head: number | null;
+//   tail: List | null;
+// }
 
-// type PairFn = (a: number | Pair, b: number | Pair) => Pair;
-
-// const pair: PairFn = (a, b) => ({ head: a, tail: b });
-
+// cons
 function pair(a: Pair["head"], b: Pair["tail"]): Pair {
   return {
     head: a,
     tail: b,
   };
 }
+
+// car
+function head(p: number | Pair | null): number | Pair | null {
+  return typeof p === "number" || p === null ? p : p.head;
+}
+
+// cdr
+function tail(p: number | Pair | null): number | Pair | null {
+  return typeof p === "number" || p === null ? p : p.tail;
+}
+
+const x = pair(1, 2);
+const y = pair(3, 4);
+const z = pair(x, y);
+
+console.log(head(x));
+console.log(tail(x));
+
+console.log(head(head(z)));
+
+function list(...args: (Pair | number | null)[]) {
+  console.log("args: ", args);
+  function list_rec(
+    acc: Pair | null,
+    ...args_rec: (Pair | number | null)[]
+  ): Pair | null {
+    if (args_rec.length === 0) return acc;
+    console.log("args_rec[0]: ", args_rec[0]);
+    console.log("...args_rec.slice(1):", ...args_rec.slice(1));
+    return pair(
+      args_rec[0],
+      // list_rec(acc, ...args_rec.slice(0, 1)),
+      list_rec(acc, ...args_rec.slice(1)),
+    );
+
+    // else {
+    //   return pair(
+    //     list_rec(acc, ...args_rec[0]),
+    //     list_rec(acc, ...args_rec.slice(1)),
+    //   );
+    // }
+    // return args_rec.length === 0
+    //   ? acc
+    //   : pair(args_rec[0], list_rec(acc, ...args_rec.slice(1)));
+  }
+  return list_rec(null, ...args);
+}
+function print_list(l: Pair | number | null) {
+  function print_list_rec(
+    l: Pair | number | null,
+    acc: (Pair | number)[],
+  ) {
+    if (l === null) return acc;
+    const h = head(l);
+    if (typeof h === "number") {
+      return print_list_rec(tail(l), [...acc, h]);
+    }
+  }
+  const res = print_list_rec(l, []);
+  console.log(res);
+}
+// const l = list(1, list(4, 5), 3);
+const l = list(1, 2, 3, 4);
+console.log("l:", l);
+console.log("print_list");
+print_list(l);
